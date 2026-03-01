6cdd6c98549b51096e95ff271978ebfe80d9f3f7
唐海荣 <18550202789@126.com>
2026-03-01 21:17:17 +0800

    66666

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp2
         static  int k = 0;
         static void Main(string[] args)
         {
-            //测试1237899000001111122222333334444455555
+            //测试123789900000111112222233333444445555566666
             int[] arr = new int[] {100,90,80,70,60, 50,40,30,20,10,10,10 };
             int[] sortArr;
             {

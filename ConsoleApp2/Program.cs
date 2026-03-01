d359991ed81c374079d1b1c9ebf974c29c898674
thr828 <43717980+thr828@users.noreply.github.com>
2026-03-01 21:23:41 +0800

    88888

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp2
         static  int k = 0;
         static void Main(string[] args)
         {
-            //测试123789900000111112222233333444445555566666
+            //测试12378990000011111222223333344444555556666688888
             int[] arr = new int[] {100,90,80,70,60, 50,40,30,20,10,10,10 };
             int[] sortArr;
             {
